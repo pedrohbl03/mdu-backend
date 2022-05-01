4984c0e6e88db9517bc1c05ea6fd67aac9ff0d51
Pedro Lima <pedrohblima03@gmail.com>
2022-05-01 12:21:53 -0300

    Add export validations

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,2 +1,4 @@
 module.exports.authValidation = require('./auth.validation');
 module.exports.userValidation = require('./user.validation');
+module.exports.todoValidation = require('./todo.validation');
+module.exports.annotationValidation = require('./annotation.validation');
