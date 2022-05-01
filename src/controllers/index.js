622b350c7428ba889c02be5faf66a335931a4f3d
Pedro Lima <pedrohblima03@gmail.com>
2022-05-01 12:21:17 -0300

    Add export controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,2 +1,4 @@
 module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
+module.exports.todoController = require('./todo.controller');
+module.exports.annotationController = require('./annotation.controller');
